3de7022fa1a22a19822919b7ee3553f00840cc89
DairyFreeHem <forkcircuit@outlook.com>
2025-10-09 14:49:16 +0300

    Added tooltip anim

--- static/personal/script.js
+++ static/personal/script.js
@@ -12,3 +12,32 @@ function setTime()
 
 setTime();
 
+document.querySelectorAll(".app-tooltip .tooltips .dropdown button").forEach((toolTipItem) => 
+{
+    toolTipItem.addEventListener("click",function(){
+        const toolTipWindow = toolTipItem.parentElement.querySelector(".dropdown-menu.show");
+        if (toolTipWindow === null )
+        {
+            return;
+        }
+        setTimeout(300);
+        const maxHeight = toolTipWindow.getBoundingClientRect().height;
+        let id = null;
+        clearInterval(id);
+        let pos = 0;
+        id = setInterval(() =>
+        {
+            if(pos >= maxHeight)
+            {
+                clearInterval(id);
+            }
+            else
+            {
+                pos += 2;
+                toolTipWindow.style.height = pos + 'px';
+            }
+
+        },2 );
+
+    });
+});
