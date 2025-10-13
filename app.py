50a926a9f1474871c9582e7d5c464f88c465571e
DairyFreeHem <forkcircuit@outlook.com>
2025-10-13 16:41:30 +0300

    Added basic factory and changed endpoint

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import pathlib
 from flask import Flask
 from personal import Personal
 
 
-app = Flask(__name__, template_folder=None)
-app.register_blueprint(Personal)
+def create_app(config_filename):
+    app = Flask(__name__, template_folder=None)
+    if config_filename is not None and Path(config_filename).exists():
+        app.config.from_pyfile(config_filename)
+
+    app.register_blueprint(Personal)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    # @app.route("/")
+    # def hello_world():
+    #     return "<p>Hello, World!</p>"
+
+    return app
+
+
--- personal/__init__.py
+++ personal/__init__.py
@@ -4,7 +4,7 @@ from globals import ROOT_PATH
 
 
 PERSONAL_ENDPOINT = 'personal'
-PERSONAL_PREFIX = '/personal'
+PERSONAL_PREFIX = '/'
 Personal = Blueprint(PERSONAL_ENDPOINT,
                     __name__,
                     url_prefix=PERSONAL_PREFIX,
